25912e9c0a0d81e25ea4c80c286775e71628e7a7
TAYYAB-PC\Tayyab <tayyabaziz5@gmail.com>
2019-07-21 00:35:43 +0500

    DB port added

--- app/config/database.config.js
+++ app/config/database.config.js
@@ -5,6 +5,7 @@ const dbConfig = config[config.environment];
 
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
+    port: dbConfig.port,
     dialect: dbConfig.dialect
 });
 
